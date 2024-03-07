6e1d2605cdcf4bb60f76a081a786e9dac228e117
Ethmane Didi Bilal <blueskytech@Ethmanes-MBP.home>
2024-03-07 15:39:17 +0100

    Refine layout structure for improved responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,44 @@
-import Header from '@/components/header'
-import './globals.css'
+import Header from '@/components/header';
+import './globals.css';
 
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import ActiveSectionContextProvider from '@/context/active-section-context'
-import { Toaster } from 'react-hot-toast'
-import Footer from '@/components/footer'
-import AppearanceChanger from '@/components/appearance-changer'
-import AppearanceContextProvider from '@/context/appearance-context'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import ActiveSectionContextProvider from '@/context/active-section-context';
+import { Toaster } from 'react-hot-toast';
+import Footer from '@/components/footer';
+import AppearanceChanger from '@/components/appearance-changer';
+import AppearanceContextProvider from '@/context/appearance-context';
 
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Ethmane Didi | Developer Portfolio',
-  description: 'Explore the developer journey of Ethmane Didi through a portfolio showcasing innovative projects and a diverse skill set in web development.',
-}
-
+  description:
+    'Explore the developer journey of Ethmane Didi through a portfolio showcasing innovative projects and a diverse skill set in web development.',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className='!scroll-smooth'>
-     <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 `}
+    <html lang="en" className="!scroll-smooth">
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-900 relative pt-28 sm:pt-36 dark:bg-gray-800 dark:text-gray-200 `}
       >
-        
-        <div className="bg-[#d0f0c0] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#749475] dark:bg-[#946263"></div>
-        <div className="bg-[#c0d0f0] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#475374]"></div>
+        <div className="bg-indigo-500/20 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-indigo-700/30"></div>
+        <div className="bg-pink-500/20 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-pink-700/30"></div>
         <AppearanceContextProvider>
-        <ActiveSectionContextProvider>
-        <Header/>
-        {children}
-        <Footer />
-        <Toaster   position="top-right"/>
-        <AppearanceChanger/>
-        </ActiveSectionContextProvider>
-      
+          <ActiveSectionContextProvider>
+            <Header />
+            {children}
+            <Footer />
+            <Toaster position="top-right" />
+            <AppearanceChanger />
+          </ActiveSectionContextProvider>
         </AppearanceContextProvider>
-        </body>
+      </body>
     </html>
-  )
+  );
 }
